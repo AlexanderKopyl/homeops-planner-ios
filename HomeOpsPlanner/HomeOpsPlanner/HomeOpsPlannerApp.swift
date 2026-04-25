167ca2d0ec285d37f7c10733019330f189b3cb6e
Oleksandr Kopyl <alexsander11115@gmail.com>
2026-04-25 12:07:24 +0300

    Init commit

---
+++ HomeOpsPlanner/HomeOpsPlanner/HomeOpsPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeOpsPlannerApp.swift
+//  HomeOpsPlanner
+//
+//  Created by Александр Копыл on 25.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct HomeOpsPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
